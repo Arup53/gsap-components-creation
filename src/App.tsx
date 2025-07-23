36936bede8b2f7578fd7e3cf1febbcc686305bb1
Arup53 <arupkumarhowlader12@gmail.com>
2025-07-23 20:41:27 +0600

    update index.css

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ import { SplitText } from "gsap/SplitText";
 gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 
 function App() {
-  return <div className="">app</div>;
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300">Hello, gsap</h1>
+    </div>
+  );
 }
 
 export default App;

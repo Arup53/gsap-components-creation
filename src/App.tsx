d4389d1f496a7402bb488dbfa03ebca824f692a6
Arup53 <arupkumarhowlader12@gmail.com>
2025-07-23 10:51:32 +0600

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
+
+function App() {
+  return <div className="">app</div>;
+}
+
+export default App;

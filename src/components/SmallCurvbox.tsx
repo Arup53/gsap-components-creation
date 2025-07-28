d2b25a216e6c44fbdc722aa27445c94ee809da48
Arup53 <arupkumarhowlader12@gmail.com>
2025-07-28 22:06:39 +0600

    implement ticker section

--- src/components/SmallCurvbox.tsx
+++ src/components/SmallCurvbox.tsx
@@ -1,23 +1,17 @@
-const SmallCurvebox = ({ icon, text }) => {
+const SmallCurvebox = ({ children, text, circleColor, className }) => {
   return (
-    <div className="h-40 w-72 px-6 py-1  gap-4 rounded-full border-2 border-purple-600 flex  items-center">
-      <div className="h-28 w-28  bg-red-500 rounded-full  flex justify-center items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-8"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z"
-          />
-        </svg>
+    <div
+      className={` h-40 w-80 py-1  gap-4 rounded-full border-2 border-purple-600 flex  items-center ${
+        className ? className : " px-6 "
+      } `}
+    >
+      <div
+        className="h-28 w-28   rounded-full  flex justify-center items-center text-4xl text-black"
+        style={{ backgroundColor: circleColor }}
+      >
+        {children}
       </div>
-      <p className="text-xl ">DTH</p>
+      <p className="text-lg ">{text}</p>
     </div>
   );
 };
--- src/components/TickerSection.tsx
+++ src/components/TickerSection.tsx
@@ -2,52 +2,220 @@ import { useGSAP } from "@gsap/react";
 import Curvebox from "./Curvebox";
 import SmallCurvebox from "./SmallCurvbox";
 
+import Marquee from "react-fast-marquee";
+
+import { CiMobile3 } from "react-icons/ci";
+import { GiSpeedometer } from "react-icons/gi";
+import { IoTvOutline, IoWaterOutline } from "react-icons/io5";
+import { LuRouter, LuSatelliteDish } from "react-icons/lu";
+import { BsCreditCard } from "react-icons/bs";
+import { col } from "motion/react-client";
+import { TbDatabaseDollar, TbHomeDollar } from "react-icons/tb";
+import { BiBox } from "react-icons/bi";
+import { GoShieldCheck } from "react-icons/go";
+
+const colorObj = {
+  grey: "#f6f6f6",
+  yellow: "#FFDFBA",
+  purple: "#DCD6FF",
+  pink: "#FFDDE3",
+  green: "#E0F5FA",
+};
+
 const TickerSection = () => {
+  console.log(colorObj.yellow);
   return (
-    <div className="flex-center">
+    <div>
       {/* // container-1 */}
 
-      <Curvebox text={"Mobile Recharge"} />
+      <Marquee>
+        <Curvebox text={"Mobile Recharge"} circleColor={colorObj.grey}>
+          <CiMobile3 />
+        </Curvebox>
 
-      <div className="mx-4 flex-center">
-        <div className="w-12 h-12 mx-2 rounded-full bg-teal-400" />
+        <div className="mx-4 flex-center">
+          <div className="w-12 h-12 mx-2 rounded-full bg-teal-400" />
 
-        <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
+          <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
 
-        <svg width="24" height="48" viewBox="15 0 30 60">
-          <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
-        </svg>
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
 
-        <svg width="24" height="48" viewBox="15 0 30 60">
-          <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
-        </svg>
-      </div>
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+        </div>
 
-      <Curvebox text={"Fast Recharge"} />
-      <Curvebox text={"Cable TV"} />
-      <div className="mx-4 flex-center">
-        <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
+        <Curvebox text={"Fast Recharge"} circleColor={colorObj.yellow}>
+          <GiSpeedometer />
+        </Curvebox>
+        <Curvebox text={"Cable TV"} circleColor={colorObj.pink}>
+          <IoTvOutline />
+        </Curvebox>
 
-        <svg width="24" height="48" viewBox="15 0 30 60">
-          <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
-        </svg>
+        <div className="mx-4 flex-center">
+          <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
 
-        <svg width="24" height="48" viewBox="15 0 30 60">
-          <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
-        </svg>
-      </div>
-      <SmallCurvebox />
-      <div className="mx-4 flex-center">
-        <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
 
-        <svg width="24" height="48" viewBox="15 0 30 60">
-          <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
-        </svg>
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+        </div>
+        <SmallCurvebox text={"DTH"} circleColor={colorObj.purple}>
+          <LuSatelliteDish />
+        </SmallCurvebox>
+        <div className="mx-4 flex-center">
+          <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
 
-        <svg width="24" height="48" viewBox="15 0 30 60">
-          <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
-        </svg>
-      </div>
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+        </div>
+      </Marquee>
+
+      {/* container-2 */}
+
+      <Marquee direction="right">
+        <div className="mx-4 flex-center">
+          <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
+
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+        </div>
+        <SmallCurvebox text={"Rent Payment"} circleColor={colorObj.yellow}>
+          <TbHomeDollar />
+        </SmallCurvebox>
+        <div className="w-12 h-12 mx-2 rounded-full bg-[#CDC5FF]" />
+        <SmallCurvebox text={"Water"} circleColor={colorObj.grey}>
+          <IoWaterOutline />
+        </SmallCurvebox>
+
+        <div className="mx-4 flex-center">
+          <div className="w-12 h-12 mx-2 rounded-full bg-[#CDC5FF]" />
+
+          <svg
+            width="24"
+            height="48"
+            viewBox="15 0 30 60"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{
+              margin: "0 8px 0 0",
+              willChange: "transform",
+            }}
+          >
+            <path d="M 45 0 a 30 30 0 0 0 0 60 z" fill="#F5BC7A" />
+          </svg>
+          <svg
+            width="24"
+            height="48"
+            viewBox="15 0 30 60"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{
+              margin: "0 8px 0 0",
+              willChange: "transform",
+            }}
+          >
+            <path d="M 45 0 a 30 30 0 0 0 0 60 z" fill="#FFDFBA" />
+          </svg>
+          <svg
+            width="24"
+            height="48"
+            viewBox="0 0 30 30"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{
+              transform: "rotate(45deg)",
+              margin: "0 12px",
+              willChange: "transform",
+            }}
+          >
+            <g style={{ mixBlendMode: "multiply" }}>
+              <rect width="30" height="30" rx="0" fill="#FFA3B5" />
+            </g>
+          </svg>
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#8085C5" />
+          </svg>
+        </div>
+        <Curvebox
+          text={"Credit Card Payment"}
+          circleColor={colorObj.purple}
+          className={"px-4"}
+        >
+          <BsCreditCard />
+        </Curvebox>
+        <Curvebox text={"Broadband"} circleColor={colorObj.green}>
+          <LuRouter />
+        </Curvebox>
+      </Marquee>
+
+      {/* container-3 */}
+
+      <Marquee>
+        <Curvebox text={"Loan Repayment"} circleColor={colorObj.yellow}>
+          <TbDatabaseDollar />
+        </Curvebox>
+
+        <div className="mx-4 flex-center">
+          <div className="w-12 h-12 mx-2 rounded-full bg-teal-400" />
+
+          <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
+
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+        </div>
+
+        <Curvebox text={"Recuring Deposit"} circleColor={colorObj.pink}>
+          <BiBox />
+        </Curvebox>
+        <Curvebox text={"Cable TV"} circleColor={colorObj.pink}>
+          <IoTvOutline />
+        </Curvebox>
+
+        <div className="mx-4 flex-center">
+          <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
+
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+        </div>
+        <SmallCurvebox text={"Insurance"} circleColor={colorObj.grey}>
+          <GoShieldCheck />
+        </SmallCurvebox>
+        <div className="mx-4 flex-center">
+          <div className="w-12 h-12 mx-2 rounded-full bg-purple-300" />
+
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+
+          <svg width="24" height="48" viewBox="15 0 30 60">
+            <path d="M 15 0 A 30 30 0 0 1 15 60 L 15 0 Z" fill="#FFA3B5" />
+          </svg>
+        </div>
+      </Marquee>
     </div>
   );
 };

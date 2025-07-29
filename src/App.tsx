f18e732b2252c57d15b2043102e3502712ff7921
Arup53 <arupkumarhowlader12@gmail.com>
2025-07-29 21:47:58 +0600

    implement subhero section

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Art from "./components/Art";
 import Test from "./components/Test";
 import TickerSection from "./components/TickerSection";
 import AdSection from "./components/AdSection";
+import SubHero from "./components/SubHero";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 
@@ -19,7 +20,7 @@ function App() {
       <Navbar />
       <Test />
       {/* <Hero /> */}
-      <Cocktails />
+      <SubHero />
       <Art />
       <About />
       <TickerSection />
---
+++ src/components/SubHero.tsx
@@ -0,0 +1,197 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { DrawSVGPlugin } from "gsap/all";
+
+gsap.registerPlugin(DrawSVGPlugin);
+
+gsap.registerPlugin(DrawSVGPlugin);
+const SubHero = () => {
+  useGSAP(() => {
+    const test1Timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#container1",
+        start: "top 90%",
+        end: "top 65%",
+        scrub: true,
+      },
+    });
+    const test1Timeline2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#container2",
+        start: "top 90%",
+        end: "top 65%",
+        scrub: true,
+        markers: true,
+      },
+    });
+    const test1Timeline3 = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#container3",
+        start: "top 90%",
+        end: "top 65%",
+        scrub: true,
+      },
+    });
+
+    test1Timeline.fromTo(
+      "#mask-path",
+      { drawSVG: "0%" },
+      { drawSVG: "100%", duration: 1, ease: "power1.out" }
+    );
+
+    test1Timeline2.fromTo(
+      "#mask-path2",
+      { drawSVG: "0%" },
+      { drawSVG: "100%", duration: 1, ease: "power1.out" }
+    );
+
+    test1Timeline3.fromTo(
+      "#mask-path3",
+      { drawSVG: "0%" },
+      { drawSVG: "100%", duration: 1, ease: "power1.out" }
+    );
+  });
+
+  return (
+    <section className="rounded-b-[3.75rem] border border-amber-400">
+      <div className="my-24 py-32 w-[50%] mx-auto h-full   ">
+        <div className="flex flex-col gap-12">
+          <svg
+            id="container1"
+            width="120"
+            height="120"
+            viewBox="0 0 52 52"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {/* Circle Background */}
+            <circle
+              cx="26"
+              cy="26"
+              r="25"
+              fill="none"
+              stroke="#007FFF"
+              strokeWidth="2"
+            />
+
+            {/* Define Mask */}
+            <mask id="checkmark-mask">
+              {/* The masked path gets drawn via GSAP */}
+              <path
+                id="mask-path"
+                d="M14 27 L22 35 L38 18"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+              />
+            </mask>
+
+            {/* This checkmark path is revealed by the mask */}
+            <path
+              d="M14 27 L22 35 L38 18"
+              fill="none"
+              stroke="#007FFF"
+              strokeWidth="2"
+              mask="url(#checkmark-mask)"
+            />
+          </svg>
+          <h3 className="text-6xl font-bold w-[60%]">
+            Faster and easier than using cards or cash.
+          </h3>
+        </div>
+
+        {/* container-2 */}
+        <div className="flex flex-col gap-12 my-24">
+          <svg
+            id="container2"
+            width="120"
+            height="120"
+            viewBox="0 0 52 52"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {/* Circle Background */}
+            <circle
+              cx="26"
+              cy="26"
+              r="25"
+              fill="none"
+              stroke="#007FFF"
+              strokeWidth="2"
+            />
+
+            {/* Define Mask */}
+            <mask id="checkmark-mask2">
+              {/* The masked path gets drawn via GSAP */}
+              <path
+                id="mask-path2"
+                d="M14 27 L22 35 L38 18"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+              />
+            </mask>
+
+            {/* This checkmark path is revealed by the mask */}
+            <path
+              d="M14 27 L22 35 L38 18"
+              fill="none"
+              stroke="#007FFF"
+              strokeWidth="2"
+              mask="url(#checkmark-mask2)"
+            />
+          </svg>
+          <h3 className="text-6xl font-bold w-[60%]">
+            Pay in full, over time, or with your card’s rewards.
+          </h3>
+        </div>
+
+        {/*  container-3 */}
+
+        <div className="flex flex-col gap-12 mt-24">
+          <svg
+            id="container3"
+            width="120"
+            height="120"
+            viewBox="0 0 52 52"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {/* Circle Background */}
+            <circle
+              cx="26"
+              cy="26"
+              r="25"
+              fill="none"
+              stroke="#007FFF"
+              strokeWidth="2"
+            />
+
+            {/* Define Mask */}
+            <mask id="checkmark-mask3">
+              {/* The masked path gets drawn via GSAP */}
+              <path
+                id="mask-path3"
+                d="M14 27 L22 35 L38 18"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+              />
+            </mask>
+
+            {/* This checkmark path is revealed by the mask */}
+            <path
+              d="M14 27 L22 35 L38 18"
+              fill="none"
+              stroke="#007FFF"
+              strokeWidth="2"
+              mask="url(#checkmark-mask3)"
+            />
+          </svg>
+          <h3 className="text-6xl font-bold w-[60%]">
+            Privacy and security built in.
+          </h3>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SubHero;

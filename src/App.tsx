54d9fcc04325737c1761e52e4b1b0f07448e85f4
Arup53 <arupkumarhowlader12@gmail.com>
2025-07-31 20:26:16 +0600

    fix import issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
-import Navbar from "./components/Navbar";
-import { Hero } from "./components/Hero";
-import Cocktails from "./components/Cocktails";
-import About from "./components/About";
-import Art from "./components/Art";
-import Test from "./components/Test";
-import TickerSection from "./components/TickerSection";
-import AdSection from "./components/AdSection";
-import SubHero from "./components/SubHero";
-import Navbar2 from "./components/Navbar2";
-import Footer from "./components/Footer";
+import Navbar2 from "./components/exchangeApp/Navbar2";
+import Test from "./components/exchangeApp/Test";
+import SubHero from "./components/exchangeApp/SubHero";
+import Art from "./components/exchangeApp/Art";
+import About from "./components/exchangeApp/About";
+import TickerSection from "./components/exchangeApp/TickerSection";
+import AdSection from "./components/exchangeApp/AdSection";
+import Footer from "./components/exchangeApp/Footer";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 

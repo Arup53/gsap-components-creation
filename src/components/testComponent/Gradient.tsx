594aa51542884bfa4d266d8ecf236b1d922c152a
Arup53 <arupkumarhowlader12@gmail.com>
2025-10-24 18:28:08 +0600

    implement solar design in graident component

--- src/components/testComponent/Gradient.tsx
+++ src/components/testComponent/Gradient.tsx
@@ -8,15 +8,9 @@ export const Gradient = () => {
       background: 'radial-gradient(48.71% 46.53% at 50% 50%, #45395c 0%, #000 100%)'
     }}
   />
+
+  <img src="/img/new/1st.webp" alt="" className='absolute top-50 left-50 translate-x-18 w-[1300px]'/>
   
-  <div className="relative z-10 flex flex-col items-center justify-center min-h-screen">
-    <h1 className="text-white text-5xl font-bold mb-4">
-      Integrate your Ledger device with numerous wallets
-    </h1>
-    <p className="text-gray-400 mb-8">
-      Ledger hardware wallet secures more than 50 software wallets
-    </p>
-    {/* Ledger device image and wallet icons */}
-  </div>
+ 
 </div>
 }

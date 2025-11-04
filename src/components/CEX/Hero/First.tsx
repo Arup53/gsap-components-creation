6fe445c49de49b4048d49b8927d6299b95aeb1e1
Arup53 <arupkumarhowlader12@gmail.com>
2025-11-04 12:20:36 +0600

    create Ad banner in hero section

--- src/components/CEX/Hero/First.tsx
+++ src/components/CEX/Hero/First.tsx
@@ -1,10 +1,23 @@
 import { motion } from "motion/react";
 import AnimatedCircles from "./AnimatedCircles";
+import { LiaArrowRightSolid } from "react-icons/lia";
 
 const First = () => {
   return (
-    <div className=" bg-white h-[100vh]">
-      <div className="flex flex-col items-center">
+    <div className=" bg-white h-[100vh] py-20">
+      <div className="w-[985px] mx-auto text-black">
+        {/* ad banner */}
+        <div className="w-[360px] px-3 py-2 rounded-full text-xs bg-[#F7F6F6] flex  item-center gap-2  ">
+          <span className="font-medium">
+            The Horizon RWA market is live on Aave
+          </span>
+          <span>Learn More</span>
+          <span className="text-base font-light">
+            <LiaArrowRightSolid />
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-0.5 text-black">
         <AnimatedCircles />
         <motion.div
           className="styles_hero_circle_platform"

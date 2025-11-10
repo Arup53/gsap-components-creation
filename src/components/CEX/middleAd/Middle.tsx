373b57a8d8281513d044d47d32bdc0727d45973e
Arup53 <arupkumarhowlader12@gmail.com>
2025-11-10 10:42:17 +0600

    implement heading section in Middle component

--- src/components/CEX/middleAd/Middle.tsx
+++ src/components/CEX/middleAd/Middle.tsx
@@ -3,8 +3,16 @@ import BouncingCircles from "./BouncingCircles";
 const Middle = () => {
   return (
     <div className="bg-white">
-      <div className="w-1/2 mx-auto ">
-        <BouncingCircles />
+      <div className="w-1/2 mx-auto border-t border-gray-200 pt-20">
+        <div className="flex flex-col justify-center items-center ">
+          <h2 className="text-[40px] text-black font-bold">Meet Aave.</h2>
+          <p className="text-xl text-gray-500 mt-2 font-medium">
+            Earn interest. Borrow when you need. 24/7
+          </p>
+        </div>
+        <div className="mt-10">
+          <BouncingCircles />
+        </div>
       </div>
     </div>
   );

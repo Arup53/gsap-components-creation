bde6ab027c52fd2932f943b351e4430a0ff44289
Arup53 <arupkumarhowlader12@gmail.com>
2025-11-13 11:35:01 +0600

    add title to HeroCEX

--- src/components/CEX/Hero/First.tsx
+++ src/components/CEX/Hero/First.tsx
@@ -5,7 +5,7 @@ import { LiaArrowRightSolid } from "react-icons/lia";
 const First = () => {
   return (
     <div className=" bg-white h-[100vh] py-20">
-      <div className="w-[985px] mx-auto text-black">
+      <div className="w-[985px] mx-auto text-black space-y-6">
         {/* ad banner */}
         <div className="w-[360px] px-3 py-2 rounded-full text-xs bg-[#F7F6F6] flex  item-center gap-2  ">
           <span className="font-medium">
@@ -15,6 +15,16 @@ const First = () => {
           <span className="text-base font-light">
             <LiaArrowRightSolid />
           </span>
+        </div>
+        <div>
+          <h1 className="text-5xl font-bold ">
+            DeFi's largest lending network.
+          </h1>
+        </div>
+        <div>
+          <p className="text-xl text-gray-500">
+            Earn, borrow, save, and swap with millions of users.
+          </p>
         </div>
       </div>
       <div className="flex flex-col items-center gap-0.5 text-black">
